64a0fdd4a6ae2813e74dea775f92708125d8afc8
Kiarash Mafi <110904374+KiarashMafi@users.noreply.github.com>
2025-08-25 11:39:54 +0330

    Add files via upload

--- producer.py
+++ producer.py
@@ -24,11 +24,10 @@ def handle_missing_values(message):
         "Vibration_Level": 0.7988757855927894,
         "Bit_Temperature": 90.02336580400343,
         "Motor_Temperature": 74.97714530662503,
-        "Maintenance_Flag": 0.05
+        "Maintenance_Flag": 0
     }
 
     max_buffer = 4
-    buffer = []
     deselected_columns = ["Timestamp", "Rig_ID", "Failure_Type", "Maintenance_Flag"]
 
     for key, value in message.items():
@@ -44,7 +43,7 @@ def handle_missing_values(message):
             elif buffer and key in buffer[-1] and not pd.isnull(buffer[-1][key]):
                 message[key] = buffer[-1][key]
             else:
-                message[key] = round(default_values[key])
+                message[key] = default_values[key]
 
     buffer.append(message)
     if len(buffer) > max_buffer:
@@ -56,22 +55,45 @@ def handle_binary_values(message):
     binary_columns = ["Pump_Status", "Compressor_Status"]
 
     for key, value in message.items():
+
         if key in binary_columns and value is not None:
             message[key] = round(value)
+        elif key in binary_columns and value is None:
+            message[key] = 1
+
         if key == "Maintenance_Flag" and value is None:
             message["Maintenance_Flag"] = 1
+        # elif key == "Maintenance_Flag" and
+
+    return message
+
+def handle_failure_type(message):
+    maintenance_flag = message["Maintenance_Flag"]
+    failure_type = 1 if not pd.isnull(message["Failure_Type"]) else 0
+
+    if maintenance_flag != failure_type:
+        message["Anomaly_Flag"] = True
+
+    if not maintenance_flag and failure_type == 0:
+        message["Maintenance_Flag"] = 1
+        message["Failure_Type"] = "Unknown_Error"
 
     return message
 
 
+def main(message):
+    output = handle_missing_values(message)
+    output = handle_binary_values(output)
+    output = handle_failure_type(output)
+    return f"After: {output}"
+
 df = pd.read_csv("output_data/rig_data_10000.csv")
+buffer = []
 
+if __name__=="__main__":
+    for _, row in df.iterrows():
+        msg_json = row.to_dict()
+        print(main(msg_json))
 
-for _, row in df.iterrows():
-    msg_json = row.to_dict()
-    # print(f"Before: {msg_json}")
-    msg_json = handle_missing_values(msg_json)
-    msg_json = handle_binary_values(msg_json)
-    print(f"After: {msg_json}")
 
 
--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 import pandas as pd
 
-# Load your CSV
 df = pd.read_csv("output_data/rig_data_10000.csv")
 
-# Compute mean of all numeric columns
-column_means = df.mean(numeric_only=True).to_dict()
+message = {'Timestamp': '2025-01-01 02:46:35', 'Rig_ID': 'RIG_01', 'Depth': 20.049031394602487, 'WOB': 1423.38156392436, 'RPM': 78.48713437623728, 
+                  'Torque': 431.7961678328709, 'ROP': 13.122869581376763, 'Mud_Flow_Rate': 1080.1095066075536, 'Mud_Pressure': 2859.746960496074, 
+                  'Mud_Temperature': 64.93723448091345, 'Mud_Density': 1184.7164068520945, 'Mud_Viscosity': 38.572940706093966, 'Mud_PH': 8.386557664203348, 
+                  'Gamma_Ray': 100.50012087340032, 'Resistivity': 16.902489806913415, 'Pump_Status': 1, 'Compressor_Status': 1, 'Power_Consumption': 194.80277238155932, 
+                  'Vibration_Level': 0.4944634824340062, 'Bit_Temperature': 90.1118772693112, 'Motor_Temperature': 74.588674808601, 'Maintenance_Flag': 1,
+                  'Failure_Type': "None"}
 
-# Print the results
-for col, mean_value in column_means.items():
-    print(f"{col}: {mean_value}")
+flag = message["Maintenance_Flag"]
+ftype = [1 if not pd.isnull(message["Failure_Type"]) else 0][0]
+print(ftype, flag)
+
+if flag != ftype:
+    message["Failure_Type"] = "Unknown_Error"
+
+print(message["Maintenance_Flag"], message["Failure_Type"])
